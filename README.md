45707d04b24a5792ff89c7521cd6f9c252dc45f2
Domen Kožar <domen@dev.si>
2015-05-24 23:09:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Flake8 support for Python files in Zed, only works in the standalone version or in Zedd projects.
+
+Based on https://github.com/zefhemel/zed-pep8.
---
+++ check.js
@@ -0,0 +1,27 @@
+/* global _ */
+var fs = require("zed/fs");
+
+module.exports = function(info) {
+    var code = info.inputs.text;
+
+    return fs.run(["flake8", "-"], code).then(function(results) {
+        var lines = results.split("\n");
+        var annos = [];
+        _.each(lines, function(line) {
+            if(!line) {
+                return;
+            }
+            var m = line.split(':', 4);
+            if (m.length === 4) {
+                annos.push({
+                    row: m[1],
+                    text: '[Flake8] ' + m[4].trim(),
+                    type: 'warning'
+                });
+            } else {
+                console.error("Invalid output from Flake8:", line);
+            }
+        });
+        return annos;
+    });
+};
