ef6d962049e16d63b6011b58f474f8f2e279c66a
Domen Kožar <domen@dev.si>
2015-05-25 11:11:33 +0200

    use correct index for message

--- check.js
+++ check.js
@@ -5,17 +5,18 @@ module.exports = function(info) {
     var code = info.inputs.text;
 
     return fs.run(["flake8", "-"], code).then(function(results) {
-        var lines = results.split("\n");
-        var annos = [];
+        var lines = results.split("\n"),
+            annos = [];
         _.each(lines, function(line) {
-            if(!line) {
+            if (!line) {
                 return;
             }
             var m = line.split(':', 4);
+
             if (m.length === 4) {
                 annos.push({
                     row: m[1],
-                    text: '[Flake8] ' + m[4].trim(),
+                    text: '[Flake8] ' + m[3].trim(),
                     type: 'warning'
                 });
             } else {
